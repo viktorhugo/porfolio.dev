df9f4ab193299aa2e0fb17ba870b5a013b09877c
viktorhugo <viktor1103@gmail.com>
2025-08-23 16:25:59 -0500

    update defineConfig astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/',
+  site: 'https://victormos.dev/',
+  output: "server",
   adapter: cloudflare()
 })
